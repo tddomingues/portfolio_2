d34449488fe378c49b392088c6f5f8769c3c87e4
tiago domingues <ttx8000@gmail.com>
2024-04-28 12:11:22 -0300

    configurado o shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./globals.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <h1 className="text-3xl font-bold underline bg-slate-600">Hello world!</h1>
   );
